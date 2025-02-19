6b30500b1120d9aa847fff600eb4c1a8375c9bd8
Simon W <simon.wittner@gmx.net>
2025-02-19 20:59:01 +0100

    float numbers short

--- src/components/results/SelfConsumptionBatteryCard.tsx
+++ src/components/results/SelfConsumptionBatteryCard.tsx
@@ -142,7 +142,7 @@ const SelfConsumptionBatteryCard = () => {
                 </Tooltip>
               </TooltipProvider>
             </div>
-            <p>Recommended Battery: <span className="font-bold">{batteryData?.evo_battery_size_kwh || 0} kWh </span> <span className="mx-2">|</span> <span className="font-bold">{batteryData?.evo_battery_size_kw || 0} kW</span></p>
+            <p>Recommended Battery: <span className="font-bold">{batteryData?.evo_battery_size_kwh.toFixed(0) || 0} kWh </span> <span className="mx-2">|</span> <span className="font-bold">{batteryData?.evo_battery_size_kw.toFixed(0) || 0} kW</span></p>
             <p>Additional Self-consumption: +{Metrics.additionalSelfConsumption.toFixed(2)}%</p>
             <p>Estimated Full Cycles per Year: {Metrics.fullCycles.toFixed(1)}</p>
           </div>
