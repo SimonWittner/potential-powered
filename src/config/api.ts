dec95fb274509e90dd86a45412759caaa6e044af
Simon W <simon.wittner@gmx.net>
2025-03-04 21:19:16 +0100

    api GCR switch

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,2 @@
-//export const API_URL = "https://google-server-843669231634.europe-west4.run.app";
-export const API_URL = "http://localhost:3001";
+export const API_URL = "https://google-server-843669231634.europe-west4.run.app";
+//export const API_URL = "http://localhost:3001";
