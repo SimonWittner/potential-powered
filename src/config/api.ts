70edf8756258b1d8ab8cde8e9ff8476d859ca11d
Simon W <simon.wittner@gmx.net>
2025-03-05 20:06:34 +0100

    api https dev.adress update

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,4 @@
 //export const API_URL = "https://google-server-843669231634.europe-west1.run.app";
 //export const API_URL = "http://localhost:3001";
-export const API_URL = "https://34.140.239.102:443";
+//export const API_URL = "https://34.140.239.102:443";
+export const API_URL = "https://dev.lumeraenergy.de";
