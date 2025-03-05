773697a927d2add599ffaa9473f4d35b68cca46b
Simon W <simon.wittner@gmx.net>
2025-03-05 19:09:13 +0100

    api https update

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,3 @@
 //export const API_URL = "https://google-server-843669231634.europe-west1.run.app";
 //export const API_URL = "http://localhost:3001";
-export const API_URL = "http://34.76.96.241:3001";
+export const API_URL = "https://34.140.239.102:443";
