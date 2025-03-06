f364e6c9d5c40e512eda88e15809b6d412af0379
Simon W <simon.wittner@gmx.net>
2025-03-06 18:20:16 +0100

    domain

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
 //export const API_URL = "https://google-server-843669231634.europe-west1.run.app";
 //export const API_URL = "http://localhost:3001";
 //export const API_URL = "https://34.140.239.102:443";
-export const API_URL = "https://dev.lumeraenergy.de";
+export const API_URL = "https://dev.lumeraenergy.de"; // external IP of GCE is connected to subdomain on IONOS.
